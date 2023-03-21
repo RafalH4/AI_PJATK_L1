443e762804044804615b42b0665c21057be10f8b
rafal <rafal@RHLATITUDE5431>
2023-03-21 15:02:51 +0100

    Add project files.

---
+++ PJATK1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PJATK1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
